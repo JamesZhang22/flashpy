a2c551ca35c5935caa5d6c258df026ea364915a4
JamesZhang22 <jameszhangjunjie@gmail.com>
2021-08-14 15:21:55 -0400

    more colors

--- utils/settings.py
+++ utils/settings.py
@@ -6,10 +6,13 @@ pygame.font.init()
 # Colors
 BLACK = (0, 0, 0)
 GRAY = (228, 228, 228)
+DARK_GRAY = (185, 185, 185)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
+LIGHT_GREEN = (110, 255, 110)
 BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
 CYAN = (131, 247, 236)
 
 """Default Settings"""
