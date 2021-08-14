94699c9bf75041cd47e4b77bf532a427162a827c
JamesZhang22 <jameszhangjunjie@gmail.com>
2021-08-14 15:21:46 -0400

    hover method

--- utils/button.py
+++ utils/button.py
@@ -1,4 +1,3 @@
-from pygame import font # Useful?
 from .settings import *
 
 class Button:
@@ -12,9 +11,23 @@ class Button:
         self.text = text
         self.text_color = text_color
         self.image = image
+        self.hover = False
 
     def draw(self, screen):
-        pygame.draw.rect(screen, self.color, (self.x, self.y, self.w, self.h))
+        if self.image == "images/add.png":
+            pygame.draw.rect(screen, self.color, (self.x, self.y, self.w, self.h), 0, 10)
+        elif self.image != "images/add.png":
+            pygame.draw.rect(screen, self.color, (self.x, self.y, self.w, self.h))
+
+        if self.hover and self.text == "START":
+            self.color = LIGHT_GREEN
+        elif not self.hover and self.text == "START":
+            self.color = GREEN
+
+        if self.hover and self.image == "images/add.png":
+            self.color = DARK_GRAY
+        elif not self.hover and self.image == "images/add.png":
+            self.color = GRAY
         
         if self.text:
             myfont = get_font_bold(self.font_size)
@@ -25,7 +38,7 @@ class Button:
             self.image_load = pygame.image.load(self.image)
             screen.blit(self.image_load, (self.x + (self.w - self.image_load.get_width()) / 2, self.y + (self.h - self.image_load.get_height()) / 2))
 
-    def clicked(self, pos) -> bool:
+    def clicked(self, pos):
         x, y = pos
 
         if not (x >= self.x and x <= self.x + self.w):
@@ -35,3 +48,15 @@ class Button:
             return False
         
         return True
+
+    def is_hover(self, pos):
+        x, y = pos
+
+        if not (x >= self.x and x <= self.x + self.w):
+            self.hover = False
+
+        elif not (y >= self.y and y <= self.y + self.h):
+            self.hover = False
+        
+        else:
+            self.hover = True
