f52725342d82b5f7ef945b76ea5df5b16b56eadb
JamesZhang22 <jameszhangjunjie@gmail.com>
2021-08-14 12:24:09 -0400

    button class

---
+++ utils/button.py
@@ -0,0 +1,37 @@
+from pygame import font # Useful?
+from .settings import *
+
+class Button:
+    def __init__(self, x, y, w, h, color, font_size, text = None, text_color = BLACK, image = None):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.color = color
+        self.font_size = font_size
+        self.text = text
+        self.text_color = text_color
+        self.image = image
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.w, self.h))
+        
+        if self.text:
+            myfont = get_font_bold(self.font_size)
+            text_surface = myfont.render(self.text, True, self.text_color)
+            screen.blit(text_surface, (self.x + self.w / 2 - text_surface.get_width() / 2, self.y + self.h / 2 - text_surface.get_height() / 2))
+        
+        if self.image:
+            self.image_load = pygame.image.load(self.image)
+            screen.blit(self.image_load, (self.x + (self.w - self.image_load.get_width()) / 2, self.y + (self.h - self.image_load.get_height()) / 2))
+
+    def clicked(self, pos) -> bool:
+        x, y = pos
+
+        if not (x >= self.x and x <= self.x + self.w):
+            return False
+
+        if not (y >= self.y and y <= self.y + self.h):
+            return False
+        
+        return True
